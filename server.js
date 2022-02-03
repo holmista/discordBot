0b8e0af8018614cf8bfbab3121d82010412d2087
Tornike Buchukuri <tornike.buchukuri.314@gmail.com>
2022-02-04 02:41:00 +0400

    added commands and tested them

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const { Client, Intents } = require('discord.js')
-// const app = express()
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
 
-console.log(Intents)
-// console.log(process.env.TOKEN)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// app.listen(process.env.PORT, ()=>console.log(`listening to port ${process.env.PORT}`))
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
+  }
+});
+
+client.login(process.env.TOKEN);
