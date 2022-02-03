55d40c3677bc5a611fb02aeee604dd116f80cf2b
holmista <mishadaro21@gmail.com>
2022-02-03 23:31:23 +0400

    set up discord bot project

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const { Client, Intents } = require('discord.js')
+// const app = express()
+
+console.log(Intents)
+// console.log(process.env.TOKEN)
+
+// app.listen(process.env.PORT, ()=>console.log(`listening to port ${process.env.PORT}`))
