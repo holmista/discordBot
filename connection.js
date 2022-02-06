eb867b60d8000d58101e710dca7b4e026bf97069
Tornike Buchukuri <tornike.buchukuri.314@gmail.com>
2022-02-07 00:07:09 +0400

    changed connection method

--- connection.js
+++ connection.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const connectionString = `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PGPORT}/${process.env.DATABASE}`;
+
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   database: process.env.DATABASE,
+//   ssl: { rejectUnauthorized: false },
+// });
+
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  database: process.env.DATABASE,
+  connectionString: connectionString,
   ssl: { rejectUnauthorized: false },
 });
 
